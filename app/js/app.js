f19c83396c4b9301d367a456aa1a25a1262ff6e2
Niru Maheswaranathan <nirum@stanford.edu>
2013-05-03 16:04:28 -0700

    basic connection with firebase set up

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,10 @@
 
 /* App Module */
 
-angular.module('tman', ['tmanServices', 'tmanFilters']).
+angular.module('tman', ['tmanServices', 'tmanFilters', 'firebase']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
+      when('/chat', {templateUrl: 'partials/chat.html',   controller: ChatCtrl}).
       when('/projects', {templateUrl: 'partials/project-list.html',   controller: ProjectListCtrl}).
       when('/projects/:projectname', {templateUrl: 'partials/project-detail.html', controller: ProjectDetailCtrl}).
       otherwise({redirectTo: '/projects'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,8 @@
 
 /* Controllers */
 
-function ProjectListCtrl($scope, $http, Project) {
-	 $scope.metadata = Project.query();
+function ProjectListCtrl($scope, Project) {
+   $scope.metadata = Project.query();
 }
 
 function ProjectDetailCtrl($scope, $routeParams, Project) {
@@ -11,4 +11,18 @@ function ProjectDetailCtrl($scope, $routeParams, Project) {
    $scope.timestamps = Project.get({
       name: 'projects/' + $routeParams.projectname
    })
+}
+
+function ChatCtrl($scope, $timeout, angularFireCollection) {
+   $scope.header = "Chat";
+   var url = 'https://tman.firebaseio.com/chat';
+   $scope.messages = angularFireCollection(url);
+   $scope.username = 'Guest' + Math.floor(Math.random() * 101);
+   $scope.addMessage = function() {
+      $scope.messages.add({
+         from: $scope.username,
+         content: $scope.message
+      });
+      $scope.message = "";
+   }
 }
