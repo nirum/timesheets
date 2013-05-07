049bac878a37b6307325aea593480f175267d345
Niru Maheswaranathan <nirum@stanford.edu>
2013-05-07 17:40:38 -0400

    working on login/authentication

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,29 @@
 /* App Module */
 
 angular.module('tman', ['tmanServices', 'tmanFilters', 'firebase']).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/', {templateUrl: 'partials/home.html'}).
-      when('/chat', {templateUrl: 'partials/chat.html',   controller: ChatCtrl}).
-      when('/firebase', {templateUrl: 'partials/tman.html',   controller: TmanCtrl}).
-      when('/projects', {templateUrl: 'partials/project-list.html',   controller: ProjectListCtrl}).
-      when('/projects/:projectname', {templateUrl: 'partials/project-detail.html', controller: ProjectDetailCtrl}).
-      otherwise({redirectTo: '/'});
+config(['$routeProvider', function($routeProvider) {
+   $routeProvider.
+   when('/', {
+      templateUrl: 'partials/home.html',
+      controller: UserCtrl
+   }).
+   when('/chat', {
+      templateUrl: 'partials/chat.html',
+      controller: ChatCtrl
+   }).
+   when('/firebase', {
+      templateUrl: 'partials/tman.html',
+      controller: TmanCtrl
+   }).
+   when('/projects', {
+      templateUrl: 'partials/project-list.html',
+      controller: ProjectListCtrl
+   }).
+   when('/projects/:projectname', {
+      templateUrl: 'partials/project-detail.html',
+      controller: ProjectDetailCtrl
+   }).
+   otherwise({
+      redirectTo: '/'
+   });
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -18,13 +18,45 @@ function TmanCtrl($scope, $timeout, angularFireCollection) {
    $scope.projects = angularFireCollection(url);
    $scope.projectname = "";
    $scope.addTime = function() {
-			var now = new Date().getTime();
+      var now = new Date().getTime();
       $scope.projects.add({
          project: $scope.projectname,
          timestamp: now
       });
       $scope.projectname = "";
    }
+}
+
+function UserCtrl($scope, $timeout, angularFire) {
+   var ref = new Firebase('https://tman.firebaseio.com');
+   var authClient = new FirebaseAuthClient(ref, function(err, user) {
+			 if (err) {
+					 console.log('err!');
+					 console.log(err);
+					 console.log(JSON.stringify(err));
+			 }
+			 else {
+					 if (user) {
+							 console.log('logged in!');
+					 }
+					 else {
+							 console.log('logged out!');
+					 }
+			 }
+			//if (!err && user) {
+			//angularFire(ref, $scope, "test");
+			//}
+   });
+   //$scope.email = "";
+   //$scope.password = "";
+   //$scope.rememberme = true;
+   //$scope.login = function() {
+      //auth.login('password', {
+         //email: $scope.email,
+         //password: $scope.password,
+         //rememberMe: $scope.rememberme
+      //});
+   //}
 }
 
 function ChatCtrl($scope, $timeout, angularFireCollection) {
